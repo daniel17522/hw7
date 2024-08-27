08b64713f8722fe36172a1ee427e144438c826c0
daniel17522 <daniel312123312@gmail.com>
2024-08-27 18:28:54 +0300

    mih

--- hw7.py
+++ hw7.py
@@ -6,6 +6,8 @@ with sqlite3.connect('users.db') as connection:
     name TEXT NOT NULL,
     salary INTEGER NOT NULL
     )''')
+    cursor.execute('''INSERT INTO employees(name, salary)
+        VALUES ('tema',20000), ('azret', 1500),('danya', 2000), ('elmir',3000), ('elmirza',4000),('emir',6000),('ela',7000),('vasya',8000), ('dima',9000), ('akilbek', 10000)''')
     cursor.execute('''SELECT * FROM employees''')
     for row in cursor.fetchall():
         print(row)
